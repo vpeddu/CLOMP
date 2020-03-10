d2de3b58172855b755db19e1a73183155311c4e2
Vikas Peddu <Vpeddu@uw.edu>
2020-03-10 11:48:00 -0700

    Update fasta_extract.sh

--- fasta_extract.sh
+++ fasta_extract.sh
@@ -7,7 +7,7 @@ cat SC5683_S2_L001_R1_001_assignments.txt | grep "\t147711" | cut -f1,3 | tr "M0
 #Rhinovirus C reads
 cat SC5698_S4_L001_R1_001_assignments.txt | grep "\t463676" | cut -f1,3 | tr "M0" ">M0" | tr "\t" "\n" > RhVC.fasta
 #moraxella reads 
-cat SC5688_S3_L001_R1_001_assignments.txt| grep "\t480" | cut -f1,3 | tr "M0" ">M0" | tr "\t" "\n" > MC_WA6_UW3.fasta
+cat 5780_S28_L001_R1_001_assignments.txt| grep "\t480" | cut -f1,3 | tr "M0" ">M0" | tr "\t" "\n" > MC_WA6_UW3.fasta
 cat sc5727_S26_L001_R1_001_assignments.txt| grep "\t480" | cut -f1,3 | tr "M0" ">M0" | tr "\t" "\n" > MC_WA9_UW6.fasta
 cat sc5746_S25_L001_R1_001_assignments.txt | grep "\t480" | cut -f1,3 | tr "M0" ">M0" | tr "\t" "\n" > MC_WA8_UW5.fasta
 
